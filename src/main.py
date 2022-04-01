e83f233b258805c66c654ebe6057a812ea2f2362
Manuelodric <69173390+Manuelodric@users.noreply.github.com>
2022-04-01 21:02:18 +0200

    Some new methods have been added.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,37 @@
 from scrape_each_book import BookScraper
+import argparse
 
-output_file = "output/test.csv"
 
-scraper = BookScraper()
-scraper.scrape()
-scraper.data2csv(output_file)
+def main(output_file, keyword):
+    """
+    Scrape todostuslibros website
+    """
+    if(keyword != ""):
+        scraper = BookScraper(
+            "https://www.todostuslibros.com/busquedas?keyword=" +
+            keyword)
+    else:
+        scraper = BookScraper()
+    scraper.scrape()
+    scraper.data2csv(output_file)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Seleccione una busqueda \
+        a realizar.')
+    parser.add_argument('-k', '--keyword', type=str, default="",
+                        help='Palabra clabe que se quiere \
+                            usar para la busqueda')
+    parser.add_argument('-o', '--output_filepath', type=str,
+                        default="output/test.csv",
+                        help='FilePath al fichero de salida')
+    args = parser.parse_args()
+
+    output_file = args.output_filepath
+    keyword = args.keyword
+
+    if(" " in keyword):
+        keyword = keyword.replace(" ", "+")
+
+    main(output_file, keyword)
