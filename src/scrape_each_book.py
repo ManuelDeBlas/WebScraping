c7c88ff72d6b822d5253042c4ccac92b1e12f920
Manuelodric <69173390+Manuelodric@users.noreply.github.com>
2022-04-06 19:46:19 +0200

    Some new methods have been added.

--- src/scrape_each_book.py
+++ src/scrape_each_book.py
@@ -7,6 +7,16 @@ import string
 
 
 class BookScraper():
+    """
+    A class that scrapes the website todostuslibros.
+    
+    Attributes:
+        This class does not have public attributes
+
+    Methods:
+        scrape (): Scrapes the website
+        data2csv (output_file): Turns the data into a csv file to the path given
+        """
     _id = 0
     _dt = []
     _headers = {
@@ -30,26 +40,47 @@ class BookScraper():
     @classmethod
     def _generate_unique_id(cls, value):
         """
-        Sums value to class variable _id
+        Sums value to class variable _id.
+
+        Parameters:
+            value (int): The value of the last id
         """
         cls._id += value
 
     def _get_html(self, url):
         """
         Gets an html from a url given.
+
+        Parameters:
+            url (string): The url of the website the html is extracted
+
+        Returns:
+            The html from the url given
         """
         return requests.get(url, headers=self._headers)
 
     def _get_book_url(self, book_row):
         """
         Gets book url
+
+        Parameters:
+            book_row (object 'bs4.element.Tag'): The information of a book.
+
+        Returns:
+            book_url (string): The book's url from the object given.
         """
         book_url = book_row.find(class_="title").a['href']
         return book_url
 
     def _get_pages_links(self, soup):
         """
-        Gets pages links
+        Gets pages links from an html given.
+
+        Parameters:
+            soup (object 'bs4.BeautifulSoup'): An html object of the first group of books.
+
+        Returns:
+            pages_urls (list): A list of the different url from all the pages where are books.
         """
         pages_urls = []
         page_items = soup.findAll('a', attrs={'class': 'page-link'})
@@ -72,74 +103,221 @@ class BookScraper():
 
     def _get_title(self, book):
         """
-        Extracts the book title and gives it format.
+        Extracts the book title.
+
+        Parameters:
+            book (object 'bs4.BeautifulSoup'): The information of a book in html
+
+        Returns:
+            title (string): The title of the book given
         """
-        title = book.find(class_="title").contents[1].contents[0]
-        return title.strip()
+        try:
+            title = book.find(class_="title").contents[0].strip()
+        except:
+            title = ""
+        return title
 
     def _get_subtitle(self, book):
         """
         Extracts the book subtitle.
-        """
-        # There might be no subtitles
-        try:
-            subtitle = book.find(class_="subtitle").contents[0].contents[0]
 
+        Parameters:
+            book (object 'bs4.BeautifulSoup'): The information of a book in html
+
+        Returns:
+            subtitle (string): The subtitle of the book given
+        """
+        try:
+            subtitle = book.find(class_="subtitle").contents[0]
         except IndexError:
             subtitle = ""
         return subtitle.strip()
-
+    
     def _get_author(self, book):
         """
         Extracts the book author.
-        """
-        autor = book.find(class_="author").contents[0].contents[0]
-        return autor.strip()
 
-    def _get_synopsis(self, book):
-        """
-        Extracts the book synopsis.
-        """
-        # TODO: Descartarlo? las sinopsis completas ocupan demasiado
-        return book.find(
-            class_="synopsis d-none d-md-block " +
-            "d-lg-block d-xl-block").contents[0]
+        Parameters:
+            book (object 'bs4.BeautifulSoup'): The information of a book in html
 
-    def _get_editorial_and_ISBN(self, book):
+        Returns:
+            author (string): The author of the book given
         """
-        Returns the editorial and the ISBN in a list.
+        try:
+            author = book.find(class_="author").contents[0].contents[0].strip()
+        except:
+            author = ""
+        return author
+
+    def _get_matter (self, book):
         """
-        ed_isbn = book.find(class_="data").contents[0].split("/")
-        # Deleting all unwanted whitespcaes
-        return [s.strip() for s in ed_isbn]
+        Extracts the book matter.
+
+        Parameters:
+            book (object 'bs4.BeautifulSoup'): The information of a book in html
+
+        Returns:
+            matter (string): The matter of the book given
+        """
+        try:
+            matter = book.find(class_="col-12 col-sm-12").contents[1].contents[1].contents[1].contents[3].contents[1].contents[0].strip()
+        except:
+            matter = ""
+        return matter
+
+    def _get_editorial(self, book):
+        """
+        Extracts the book editorial.
+
+        Parameters:
+            book (object 'bs4.BeautifulSoup'): The information of a book in html
+
+        Returns:
+            editorial (string): The editorial of the book given
+        """
+        try:
+            editorial = book.find(class_="col-12 col-sm-12").contents[3].contents[1].contents[1].contents[2].contents[0].contents[0].strip()
+        except:
+            editorial = ""
+        return editorial
+
+    def _get_collection(self, book):
+        """
+        Extracts the book collection.
+
+        Parameters:
+            book (object 'bs4.BeautifulSoup'): The information of a book in html
+
+        Returns:
+            collection (string): The collection of the book given
+        """
+        try:
+            collection = book.find(class_="col-12 col-sm-12").contents[3].contents[1].contents[1].contents[6].contents[0].strip()
+        except:
+            collection = ""
+        return collection
+
+    def _get_binding(self, book):
+        """
+        Extracts the book binding.
+
+        Parameters:
+            book (object 'bs4.BeautifulSoup'): The information of a book in html
+
+        Returns:
+            binding (string): The binding of the book given
+        """
+        try:
+            binding = book.find(class_="col-12 col-sm-12").contents[3].contents[1].contents[1].contents[9].contents[0].strip()
+        except:
+            binding = ""
+        return binding
+
+    def _get_book_feature(book, c1, c2):
+        """
+        Extracts a book feature for 2 parameters given. The parameters depend on the feature to look for.
+
+        Parameters:
+            book (object 'bs4.BeautifulSoup'): The information of a book in html
+            c1 (integer): A number to look for inside the html
+            c2 (integer): Another number to look for inside an html
+
+        Returns:
+            feature (string): The feature of the book given
+        """
+        try:
+            feature = book.find(class_="col-12 col-sm-12").contents[3].contents[c1].contents[1].contents[c2].contents[0].strip()
+        except:
+            feature = ""
+        return feature
 
     def _get_price(self, book):
         """
         Extracts the book price.
+
+        Parameters:
+            book (object 'bs4.BeautifulSoup'): The information of a book in html
+
+        Returns:
+            price (list): The price of the book given
         """
         price = book.find(class_="book-price").contents[1].contents[0]
         # Digits are extracted from the string
-        return re.findall(r'\d+\.\d+|\d+', price)[0]
+        return re.findall(r'\d+\.\d+|\d+', price)
 
     def _get_price_no_taxes(self, book):
         """
         Extracts the book price without taxes.
-        """
-        untaxed = book.find(class_="book-price").contents[2]
-        # Digits are extracted from the string
-        return re.findall(r'\d+\.\d+|\d+', untaxed)[0]
 
-    def _get_book_image(self, book):
+        Parameters:
+            book (object 'bs4.BeautifulSoup'): The information of a book in html
+
+        Returns:
+            untaxed price (list): The price without taxes of the book given
         """
-        Extract book image
+        try:
+            untaxed = book.find(class_="book-price-alternative").contents[0]
+            # Digits are extracted from the string
+            untaxed = re.findall(r'\d+\.\d+|\d+', untaxed)
+        except AttributeError:
+            untaxed = ['']
+        return untaxed
+
+    def _get_book_cover(self, book):
         """
-        # TODO: Representa que con extraer el link es suficiente?
-        img = book.find(class_="book-image col-3 col-sm-3 col-md-2")
-        return img.a.img['src']
+        Extracts the book cover.
+
+        Parameters:
+            book (object 'bs4.BeautifulSoup'): The information of a book in html
+
+        Returns:
+            cover (string): The url of the cover's picture
+        """
+        try:
+            cover = book.find(class_="portada").attrs['src'] 
+        except AttributeError:
+            cover = ""
+        return cover
+
+    def _get_book_info(self, book):
+        """
+        Extracts the book information.
+
+        Parameters:
+            book (object 'bs4.BeautifulSoup'): The information of a book in html
+        
+        Returns:
+            book_dict (dict): The book parameters
+        """
+        return {"id": self._id, 
+        "title": self._get_title(book),
+        "subtitle": self._get_subtitle(book),
+        "author": self._get_author(book),
+        "matter": self._get_matter(book),
+        "editorial": self._get_editorial(book),
+        "collection": self._get_collection(book),
+        "binding": self._get_binding(book),
+        "country": self._get_book_feature(book, 1, 12),
+        "lenguage of publication": self._get_book_feature(book, 1, 15),
+        "original lenguage": self._get_book_feature(book, 1, 18),
+        "ISBN": self._get_book_feature(book, 3, 2),
+        "EAN": self._get_book_feature(book, 3, 6),
+        "dimension": self._get_book_feature(book, 3, 9),
+        "weight": self._get_book_feature(book, 3, 13),
+        "number of pages": self._get_book_feature(book, 3, 17),
+        "publication date": self._get_book_feature(book, 3, 20),
+        "price (€)": self._get_price(book),
+        "untaxed price (€)": self._get_price_no_taxes(book),
+        "book cover": self._get_book_cover(book)
+        }
 
     def _get_books(self, soup, delay):
         """
-        Extracts the information from all books.
+        Extracts the information from all books and appends them to the object.
+
+        Parameters:
+            soup (object 'bs4.BeautifulSoup'): An html of a page of different books given
+            delay (float): The time it gets to request an html
         """
         book_info = dict()
         books = soup.find_all(class_="book row")
@@ -153,28 +331,18 @@ class BookScraper():
             # Increase unique id by modifiying private variable _id
             self._generate_unique_id(1)
 
-            book_info = {"id": self._id}
+            #book_info = {"id": self._id}
             # TODO: Reescribir las funciones de abajo para que extraigan la información de la pagina del libro.
-            """
-            book_info = {"id": self._id,
-                         "title": self._get_title(soup),
-                         "subtitle": self._get_subtitle(soup),
-                         "author": self._get_author(soup),
-                         "synopsis": self._get_synopsis(soup),
-                         "editorial": self._get_editorial_and_ISBN(soup)[0],
-                         "ISBN": self._get_editorial_and_ISBN(soup)[1],
-                         "price (€)": self._get_price(soup),
-                         "untaxed price (€)": self._get_price_no_taxes(soup),
-                         "book cover": self._get_book_image(soup)
-                         }
-            """
+            
+            book_info = self._get_book_info(soup)
+            
             self._dt.append(book_info)
             # Force delay between html requests:
             time.sleep(10 * delay)
 
     def scrape(self):
         """
-        Scraps the web.
+        Scrapes the web.
         """
         print("Web Scraping of books data from {} ".format(self.url) +
               "This process could take about x minutes.\n")
@@ -190,17 +358,19 @@ class BookScraper():
 
         soup = BeautifulSoup(html_page.content, features="html.parser")
         self._get_books(soup, response_delay)
-        """
+
         # Loop through all pages and get their relevant content
         for page in self._get_pages_links(soup):
             html_page = self._get_html(page)
             soup = BeautifulSoup(html_page.content, features="html.parser")
-            self._get_books(soup)
-        """
+            self._get_books(soup, response_delay)
 
     def data2csv(self, output_file):
         """
         Turns the data into a csv file to the path given.
+
+        Parameters:
+            output_file (string): File path for the output.
         """
         with open(output_file, 'w', encoding='utf-8') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=self._dt[0].keys())
